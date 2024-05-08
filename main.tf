a12430a838c5c733ee68df5aeee0fc7f79d2a264
alhusseinahmed <60324891+alhusseinahmed@users.noreply.github.com>
2024-05-09 01:13:04 +0200

    Update Terraform code in main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,52 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+# To Generate Private Key
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+}
+
+# Create Key Pair for Connecting EC2 via SSH
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+# Save PEM file locally
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa_4096.private_key_pem
+  filename = var.key_name
+}
+
+resource "aws_security_group" "ec2_security_group" {
+  name        = "ec2_security_group"
+  description = "Security group for EC2"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 resource "aws_instance" "ec2-instance" {
   ami           = "ami-098c93bd9d119c051"
   instance_type = "t2.micro"
+  key_name      = aws_key_pair.key_pair.key_name
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
 
   tags = {
     Name = "First-Instance"
