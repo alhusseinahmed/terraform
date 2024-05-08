0ba9fe5228af9ae1188f41a395fe3de394b989e0
alhusseinahmed <60324891+alhusseinahmed@users.noreply.github.com>
2024-05-09 01:19:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_key_pair" "key_pair" {
 
 resource "local_file" "private_key" {
   content  = tls_private_key.rsa_4096.private_key_pem
-  filename = var.key_name
+  filename = "key_pair"
 }
 
 resource "aws_security_group" "ec2_security_group" {
