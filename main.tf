271b2b3715d6c1c0672197814f94d583c4930362
alhusseinahmed <60324891+alhusseinahmed@users.noreply.github.com>
2024-05-09 01:18:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,23 +12,16 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-# To Generate Private Key
 resource "tls_private_key" "rsa_4096" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-variable "key_name" {
-  description = "Name of the SSH key pair"
-}
-
-# Create Key Pair for Connecting EC2 via SSH
 resource "aws_key_pair" "key_pair" {
-  key_name   = var.key_name
+  key_name   = "key_pair"
   public_key = tls_private_key.rsa_4096.public_key_openssh
 }
 
-# Save PEM file locally
 resource "local_file" "private_key" {
   content  = tls_private_key.rsa_4096.private_key_pem
   filename = var.key_name
