3748c89e2ca9de36e9ae4c7e81a4eedaa22e8b01
alhusseinahmed <alhusseinahmad1@gmail.com>
2024-05-10 01:56:01 +0200

    add user data file

---
+++ user_data.sh
@@ -0,0 +1,8 @@
+sudo yum update -y
+sudo service docker start
+sudo service docker status
+sudo groupadd docker
+sudo usermod -a -G docker ec2-user
+newgrp docker
+docker pull alhusseinahmed/go-webapp-sample
+docker run -d -p 8080:8080 alhusseinahmed/go-webapp-sample
